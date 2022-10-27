926ff5c4a21a915634dc39a3824ef9fa8da23f65
TurchinMV <ddd@sss.rr>
2022-10-27 14:23:02 +0300

    Чистка проекта

--- src/main/java/Less_1/Task_1122.java
+++
@@ -1,8 +0,0 @@
-package Less_1;
-
-public class Task_1122 {
-    public static void main(String[] args) {
-        System.out.println("Вторая задача решена");
-    }
-
-}
--- src/main/java/New_main.java
+++
@@ -1,5 +0,0 @@
-public class New_main {
-    public static void main(String[] args) {
-        System.out.println("Пусть всегда будет солнце!!!");
-    }
-}
