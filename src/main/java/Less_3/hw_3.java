b4c3e4b47e37b07ebf682f0a0fb0c8f4528b30ef
stavrtin <stavrtin@yandex.ru>
2022-10-29 18:59:25 +0300

    Check

--- src/main/java/Less_3/hw_3.java
+++ src/main/java/Less_3/hw_3.java
@@ -2,7 +2,7 @@ package Less_3;
 
 public class hw_3 {
     public static void main(String[] args) {
-        System.out.println("Chack commit");
+        System.out.println("Check commit");
 
     }
 }
