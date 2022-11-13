10519224ed9a9b46d87a1472dae0b247f3ea2d56
stavrtin <stavrtin@yandex.ru>
2022-11-13 14:58:24 +0300

    Задача про скобки

---
+++ src/main/java/less_4/task20.java
@@ -0,0 +1,60 @@
+package less_4;
+
+import java.util.Stack;
+
+public class task20 {
+    public static void main(String[] args) {
+        System.out.println("Задача 20");
+        String s = "(([])){}";
+        System.out.println(s);
+        System.out.println(isValid(s));
+
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        // ---- сложим строку в стек -------
+        for (int i = 0; i < s.length(); i++){
+            // ---- если открывающая - закидываем на стек -----
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' ||s.charAt(i) == '{' ){
+                stack.add(s.charAt(i));
+            }
+            // ---- если стек пустой - сразу FALSE, тк значит, что пришла закрывающая, а открывающей в стеке нет--
+            else if (stack.isEmpty()) {
+                return false;
+
+            } else // ---- если не открывающаяся - читаем из стека ----
+
+                switch (s.charAt(i)){
+                    case (')'): {
+                        if (stack.peek() != '(') return false;
+                        stack.pop();
+                        break;
+                    }
+                    case (']'): {
+                        if (stack.peek() != '[') return false;
+                        stack.pop();
+                        break;
+                    }
+                    case ('}'): {
+                        if (stack.peek() != '{') return false;
+                        stack.pop();
+                        break;
+                    }
+                    default: stack.pop();
+                }
+
+//        System.out.println(stack);
+
+        }
+
+        if (!stack.isEmpty()) return false;
+        return true;
+
+    }
+
+}
+
+
+
